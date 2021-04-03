ccf8a6899b7f8c7a1a2a517a05db5eed831393c7
Arvinth C <54614142+ArvinthC3000@users.noreply.github.com>
2021-04-03 11:30:00 +0530

    New header

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { BrowserRouter, Link, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { Bye } from './pages/Bye'
+import { Header } from './pages/Header'
 import { Home } from './pages/Home'
 import { Login } from './pages/Login'
 import { Register } from './pages/Register'
@@ -11,20 +12,7 @@ export const Routes: React.FC = () => {
   return <BrowserRouter>
 
     <div>
-      <header>
-        <div>
-          <Link to="/" >Home</Link>
-        </div>
-        <div>
-          <Link to="/register" >Register</Link>
-        </div>
-        <div>
-          <Link to="/login" >Login</Link>
-        </div>
-        <div>
-          <Link to="/bye" >Bye</Link>
-        </div>
-      </header>
+      <Header />
     </div>
     <Switch>
       <Route exact path="/" component={Home} />
